7c3389a2c8fdb1ecaa666adaadbb7a12869f613d
daishi <daishi@axlight.com>
2018-12-08 22:00:40 +0900

    add abort support [2]

---
+++ examples/04_abort/src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import DisplayRemoteData from './DisplayRemoteData';
+
+const App = () => (
+  <div>
+    <div>
+      <DisplayRemoteData id="1" />
+    </div>
+    <div>
+      <DisplayRemoteData id="2" />
+    </div>
+  </div>
+);
+
+export default App;
---
+++ examples/04_abort/src/DisplayRemoteData.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+import { useFetch } from 'react-hooks-fetch';
+
+const Err: React.SFC<{ error: Error }> = ({ error }) => (
+  <span>
+    Error:
+    [{error.name}]
+    {error.message}
+  </span>
+);
+
+const Loading: React.SFC<{ abort: () => void }> = ({ abort }) => (
+  <div>
+    <span>Loading...</span>
+    <button onClick={abort}>Abort</button>
+  </div>
+);
+
+const DisplayRemoteData: React.FC<{ id: string }> = ({ id }) => {
+  const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+  const { error, loading, data, abort } = useFetch(url);
+  if (error) return <Err error={error} />;
+  if (loading) return <Loading abort={abort} />;
+  return (
+    <span>
+    RemoteData:
+    {data.title}
+    </span>
+  );
+};
+
+export default DisplayRemoteData;
