517b52b92f5f8def72ece6e41e1a2c3e49ee1a05
daishi <daishi@axlight.com>
2019-10-27 11:50:34 +0900

    run compile

--- dist/index.js
+++ dist/index.js
@@ -6,13 +6,21 @@ require("core-js/modules/es.symbol.description");
 
 require("core-js/modules/es.symbol.iterator");
 
+require("core-js/modules/es.array.concat");
+
 require("core-js/modules/es.array.is-array");
 
 require("core-js/modules/es.array.iterator");
 
 require("core-js/modules/es.date.to-string");
 
+require("core-js/modules/es.object.create");
+
 require("core-js/modules/es.object.define-property");
+
+require("core-js/modules/es.object.get-prototype-of");
+
+require("core-js/modules/es.object.set-prototype-of");
 
 require("core-js/modules/es.object.to-string");
 
@@ -27,11 +35,13 @@ require("core-js/modules/web.dom-collections.iterator");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.useAsync = exports.createStatic = exports.createAsync = void 0;
+exports.useFetch = exports.initialize = exports.prefetch = exports.ErrorBoundary = void 0;
 
 require("regenerator-runtime/runtime");
 
 var _react = require("react");
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
@@ -44,6 +54,87 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var ErrorBoundary =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(ErrorBoundary, _Component);
+
+  function ErrorBoundary() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, ErrorBoundary);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(ErrorBoundary)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "state", {
+      error: null
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "retry", function () {
+      _this.setState({
+        error: null
+      });
+    });
+
+    return _this;
+  }
+
+  _createClass(ErrorBoundary, [{
+    key: "render",
+    value: function render() {
+      var error = this.state.error;
+      var _this$props = this.props,
+          children = _this$props.children,
+          fallback = _this$props.fallback;
+
+      if (error) {
+        if (typeof fallback === 'function') {
+          return fallback(error, this.retry);
+        }
+
+        return fallback;
+      }
+
+      return children;
+    }
+  }], [{
+    key: "getDerivedStateFromError",
+    value: function getDerivedStateFromError(error) {
+      return {
+        error: error
+      };
+    }
+  }]);
+
+  return ErrorBoundary;
+}(_react.Component);
+
+exports.ErrorBoundary = ErrorBoundary;
 
 var createFetchFunc = function createFetchFunc(input) {
   if (typeof input === 'function') return input;
@@ -79,7 +170,7 @@ var createFetchFunc = function createFetchFunc(input) {
   );
 };
 
-var createAsync = function createAsync(input) {
+var prefetch = function prefetch(input) {
   var fetchFunc = createFetchFunc(input);
   var state = {
     pending: true
@@ -127,9 +218,9 @@ var createAsync = function createAsync(input) {
   };
 };
 
-exports.createAsync = createAsync;
+exports.prefetch = prefetch;
 
-var createStatic = function createStatic(data) {
+var initialize = function initialize(data) {
   return {
     get data() {
       return data;
@@ -138,19 +229,19 @@ var createStatic = function createStatic(data) {
   };
 };
 
-exports.createStatic = createStatic;
+exports.initialize = initialize;
 
-var useAsync = function useAsync(initialResult) {
+var useFetch = function useFetch(initialResult) {
   var _useState = (0, _react.useState)(initialResult),
       _useState2 = _slicedToArray(_useState, 2),
       result = _useState2[0],
       setResult = _useState2[1];
 
   result.refetch = (0, _react.useCallback)(function (nextInput) {
-    var nextResult = createAsync(nextInput);
+    var nextResult = prefetch(nextInput);
     setResult(nextResult);
   }, []);
   return result;
 };
 
-exports.useAsync = useAsync;
+exports.useFetch = useFetch;
